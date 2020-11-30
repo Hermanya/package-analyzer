1542ce498abe4de81f051dcddcbeae942f04edc4
Herman Starikov <hermanstarikov@gmail.com>
2020-11-29 21:57:53 -0500

    Add a dummy test

---
+++ packages/ui/src/tests/BundleDetails.test.tsx
@@ -0,0 +1,10 @@
+import { BundleDetails } from "../components/BundleDetails";
+// import { render, screen } from "@testing-library/react";
+// import React from "react";
+
+test("BundleDetails", async () => {
+  expect(BundleDetails).toBeDefined();
+  //   render(<BundleDetails />);
+
+  //   expect(screen.getByText(/loading/i)).toBeInTheDocument();
+});
